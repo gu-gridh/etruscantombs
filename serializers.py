34f3aec5b78c449436ec424bcf6bd59736188a0a
Matteo Tomasini <matteo.tomasini@lir.gu.se>
2023-09-26 15:15:30 +0200

    Added objects counters to Place serializer

--- serializers.py
+++ serializers.py
@@ -1,5 +1,6 @@
 from diana.abstract.serializers import DynamicDepthSerializer, GenericSerializer
-from rest_framework_gis.serializers import GeoFeatureModelSerializer, ListSerializer
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework.serializers import SerializerMethodField
 from . import models
 from diana.utils import get_fields, DEFAULT_FIELDS
 from .models import *
@@ -13,22 +14,37 @@ class PlaceSerializer(DynamicDepthSerializer):
 
 
 class TIFFImageSerializer(DynamicDepthSerializer):
-
+    
     class Meta:
         model = Image
         fields = get_fields(Image, exclude=DEFAULT_FIELDS)+ ['id']
         
-
+    
 class PlaceGeoSerializer(GeoFeatureModelSerializer):
 
-    images = TIFFImageSerializer(many=True)
+    # images = TIFFImageSerializer(many=True)
+    photographs_count = SerializerMethodField()
+    plans_count = SerializerMethodField()
+    threedhop_count = SerializerMethodField()
+    pointcloud_count = SerializerMethodField()
     
     class Meta:
         model = Place
-        fields = get_fields(Place, exclude=DEFAULT_FIELDS)+ ['id', 'images']
+        fields = get_fields(Place, exclude=DEFAULT_FIELDS)+ ['id', 'photographs_count', 'plans_count', 'threedhop_count', 'pointcloud_count']
         geo_field = 'geometry'
         depth = 1
-
+            
+    def get_photographs_count(self, obj):
+        return obj.images.filter(type_of_image__text="photograph").count()
+        
+    def get_plans_count(self, obj):
+        return obj.images.filter(type_of_image__text="floor plan").count()
+    
+    def get_threedhop_count(self, obj):
+        return obj.object_3Dhop.count()
+    
+    def get_pointcloud_count(self, obj):
+        return obj.object_pointcloud.count()
 
 
 class LayerSerializer(DynamicDepthSerializer):
@@ -45,7 +61,7 @@ class AuthorSerializer(DynamicDepthSerializer):
         fields = get_fields(Author, exclude=DEFAULT_FIELDS)+ ['id']
 
 class Object3DHopSerializer(DynamicDepthSerializer):
-
+    
     class Meta:
         model = Object3DHop
         fields = get_fields(Object3DHop, exclude=DEFAULT_FIELDS)+ ['id']
